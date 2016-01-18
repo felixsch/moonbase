60a5e274460f7de03d4fd0d422532219b2acbe68
Felix Schnizlein <felix@schnizle.in>
2016-01-18 21:43:02 +0100

    Cli is working

--- src/Moonbase/Cli.hs
+++ src/Moonbase/Cli.hs
@@ -2,22 +2,22 @@
 
 module Moonbase.Cli where
 
-import Control.Monad
-import Control.Monad.Trans.Maybe
-import Control.Monad.IO.Class
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Maybe
 
-import Control.Lens hiding (argument)
-import Control.Applicative
+import           Control.Applicative
+import           Control.Lens              hiding (argument)
 
-import Options.Applicative
+import           Options.Applicative
 
-import Data.Maybe
-import Moonbase.DBus
+import           Data.Maybe
+import           Moonbase.DBus
 
 import qualified DBus
-import qualified DBus.Client as DBus
+import qualified DBus.Client               as DBus
 
-import System.Exit
+import           System.Exit
 
 data Options = Options
   { _verbose :: Bool
@@ -44,35 +44,27 @@ parseOptions = execParser full
     argsHelp    ="Arguments for a action. Show action help by help ACTION"
     actionHelp' = "Run specified action. Show all commands available with list-actions"
 
-{-
-runMoonbaseAction :: Name -> [String] -> IO (Maybe String)
-runMoonbaseAction name args' = do
-    client <- connectSession
+runCommand :: String -> [String] -> IO ()
+runCommand cmd args = do
+   client <- connectDBus moonbaseCliBusName
 
-    reply <- call_ client (methodCall (withObjectPath "Action") (withInterface "Action") (memberName_ "RunAction"))
-      { methodCallDestination = Just moonbaseBusName
-      , methodCallBody        = [toVariant (name:args')] }
+   when (isNothing client) $ die "Could not connect to DBus"
 
-    let (Just reply') = fromVariant (methodReturnBody reply !! 0)
+   reply <- DBus.call (fromJust client) (DBus.methodCall (withObjectPath "Action") (withInterface "Action") (DBus.memberName_ "RunAction"))
+     { DBus.methodCallDestination = Just moonbaseBusName
+     , DBus.methodCallBody        = [DBus.toVariant (cmd:args)]
+     }
+   case reply of
+     Left err    ->  die $ DBus.methodErrorMessage err
+     Right reply ->  case replyValue reply of
+       Nothing -> die "Could not decode moonbase reply. This indicates a bug, report this if the error persists"
+       Just x  -> putStrLn x
+   where
+     replyValue reply    = DBus.fromVariant (head $ DBus.methodReturnBody reply)
 
-    return $ if (not $ null reply')
-                then Just reply'
-                else Nothing
--}
-runCommand :: String -> [String] -> IO (Maybe String)
-runCommand cmd args = runMaybeT $ do
-   client <- liftIO $ connectDBus moonbaseCliBusName
-   return "foo"
-
-{-
 runCli :: (Bool -> IO ()) -> IO ()
 runCli start = do
   options <- parseOptions
   if options ^. cmd  == "start"
     then start (options ^. verbose)
-    else do
-      result <- runCommand (options ^. cmd) (options ^. args)
-      case result of
-        Just x  -> putStrLn x
-        Nothing -> exitFailure "Could not execute action. DBus connection failed."
--}
+    else runCommand (options ^. cmd) (options ^. args)
